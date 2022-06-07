3fdb6440446609f5def9aae62a6d012ec2030fa3
vova.chakalov.2003 <vova.chakalov.2003@gmail.com>
2022-06-07 14:18:47 +0300

    Добавил класс причалов

---
+++ MultithreadingOnShips/PierLoader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingOnShips
+{
+    public class PierLoader
+    {
+        public Tunnel Tunnel { get; }
+
+        public Types ShipType { get; }
+
+        public bool IsAlive { get; set; } = true;
+
+
+        public PierLoader(Tunnel tunnel, Types type)
+        {
+            Tunnel = tunnel;
+            ShipType = type;
+        }
+
+        public void Start()
+        {
+            while (IsAlive)
+            {
+                try
+                {
+                    Thread.Sleep(1000);
+                    Ship ship = Tunnel.GetShip(ShipType);
+                    var th = Thread.CurrentThread;
+                    if (ship != null)
+                    {
+                        while (ship.CheckCount())
+                        {
+                            Thread.Sleep(100);
+                            ship.Add(10);
+                            Console.WriteLine($"Идет загрузка {ship.Count}-{th.Name}\n");
+                        }
+                        Console.WriteLine($"Корабль завершил погрузку\nТип-{ship.Type}-Размер-{ship.Size} Название потока-{th.Name}\n\n");
+                    }
+                }
+                catch
+                {
+                }
+            }
+        }
+        public void Break()
+        {
+            IsAlive = false;
+        }
+    }
+}
