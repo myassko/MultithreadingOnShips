9a786faa4451a50096c25f4b5a83dfb92d853559
vova.chakalov.2003 <vova.chakalov.2003@gmail.com>
2022-06-07 14:09:25 +0300

    Add project files.

---
+++ MultithreadingOnShips/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingOnShips
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
